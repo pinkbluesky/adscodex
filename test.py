d7633c3650cc7b56c6db8a2788c01967f3c05a27
pinkbluesky <alina.li@outlook.com>
2022-07-26 10:25:12 -0700

    update test.py

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import subprocess
 import os
 import time
 
-data_path = os.path.expanduser('~') + "/16kfilerand"
+data_path = os.path.expanduser('~') + "/32kfilerand"
 prefix_path = os.path.expanduser('~') + "/mfes/ads"
 
-dna_path = os.path.expanduser('~') + "/dna.out" # + "/dna" + str(seed) + ".out"
+# dna_path = os.path.expanduser('~') + "/dna.out" # + "/dna" + str(seed) + ".out"
 
 def run(command):
     subprocess.run(command, shell=True) #, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -27,35 +27,41 @@ def output_path():
 
 def run_mfes(seed):
     # clear the file. run encoder
-    run("> {}".format(dna_path))
-    print("go run encode")
+    run("> {}".format(input_path()))
+    print("go run encode\n")
     run("go run encode/main.go -tbl tbl -printfmt 1 -rndmz -rndseed {} {} >> {}".format(seed, data_path, input_path()))
-    print("mfes")
+    print("mfes... beginning timer\n")
+    start = time.time()
     run("mfes -material dna -multi {}".format(prefix_path))
+    mfe_time = time.time() - start
 
+    print()
     # read the output file
     with open(output_path()) as fp:
         for line in fp:
             if line[0] != "%":
-                return float(line) # return the decimal value
+                return float(line), mfe_time # return energy value and time
 
 
 
 def main():
 
-    subprocess.run("echo running random seed tests...", shell=True)
+    run("echo running random seed tests...")
 
     seeds = [50]
     mfes = []
+    mfe_times = []
     times = []
 
     for s in seeds:
         start = time.time()
-        mfes.append(run_mfes(s))
+        mfe_energy, mfe_time = run_mfes(s)
+        mfes.append(mfe_energy)
+        mfe_times.append(mfe_time)
         times.append(time.time() - start)
         
     for i in range(len(seeds)):
-        print("seed {}: {} kcal/L, time elapsed: {} sec".format(seeds[i], mfes[i], times[i]))
+        print("seed {}: {} kcal/L\ntime elapsed: {} sec, mfe time elapsed; {} sec".format(seeds[i], mfes[i], times[i], mfe_times[i]))
 
 
 if __name__ == "__main__":
